a80946e07ab3d36eed87c691007016a11c295ee2
Tiago-Silverio-da-Costa <tiagosilveriodacosta@gmail.com>
2024-10-14 18:56:08 -0300

    Mini Music Player added

--- src/SimpleAnimation.java
+++ src/SimpleAnimation.java
@@ -18,10 +18,10 @@ public class SimpleAnimation {
         MyDrawPanel drawPanel = new MyDrawPanel();
 
         frame.getContentPane().add(drawPanel);
-        frame.setSize(600,600);
+        frame.setSize(300,300);
         frame.setVisible(true);
 
-        for (int i = 0; i < 260; i++) {
+        for (int i = 0; i < 130; i++) {
             x++;
             y++;
 
@@ -36,6 +36,9 @@ public class SimpleAnimation {
     class MyDrawPanel extends JPanel {
 
         public void paintComponent(Graphics g) {
+            g.setColor(Color.white);
+            g.fillRect(0,0,this.getWidth(),this.getHeight());
+
             g.setColor(Color.green);
             g.fillOval(x,y,40,40);
         }
